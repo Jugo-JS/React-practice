f35d1be8e04d0661d32c9d556c124adb3beef586
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-09 10:00:49 +0100

    practicing Reacto Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function BookList() {
+  return (
+  <section>
+    <Book />
+  </section>)  
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>)
+}
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="" />
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>
+
+const Author = () => <h4>Amelia Hepworth</h4>
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
