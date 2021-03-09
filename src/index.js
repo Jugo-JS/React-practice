f7178fd723ef6d1f37516d673efcf35dfe5b902c
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-09 12:57:38 +0100

    adding some props

--- src/index.js
+++ src/index.js
@@ -1,34 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// CSS
 import './index.css';
+
+// setup variables
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth'
+}
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg",
+  title: 'The Cat in the Hat',
+  author: 'Dr. Seuss'
+}
 
 function BookList() {
   return (
   <section className='booklist'>
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+    <Book 
+      img={firstBook.img} 
+      title={firstBook.title} 
+      author={firstBook.author} 
+    >
+      <p>
+        Show a child just how strong your love is every minute of the day! 
+        Features a "To" and "From" personalization page, making this sweet offering 
+        an ideal gift for baby showers, birthdays, and new parents. 
+      </p>
+    </Book>
+    <Book 
+      img={secondBook.img} 
+      title={secondBook.title} 
+      author={secondBook.author} 
+    />
   </section>)  
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author, children } =  props;
+  console.log(children);
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
-    </article>)
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
+    </article>
+  );
 }
-
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="" />
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>
-
-const Author = () => <h4>Amelia Hepworth</h4>
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
