e834b27710468ec0bee47379629dd91d5f228fa5
Jugo-JS <jugoslavsubotic@gmail.com>
2021-03-09 13:50:06 +0100

    adding component dinamicly plus spread

--- src/index.js
+++ src/index.js
@@ -5,49 +5,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // setup variables
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth'
-}
-
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg",
-  title: 'The Cat in the Hat',
-  author: 'Dr. Seuss'
-}
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth'
+  }
+  ,
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81drfTT9ZfL._AC_UL200_SR200,200_.jpg",
+    title: 'The Cat in the Hat',
+    author: 'Dr. Seuss'
+  }
+  ,
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg",
+    title: "It's Not Easy Being a Bunny",
+    author: 'Marilyn Sadler'
+  }
+];
 
 function BookList() {
   return (
   <section className='booklist'>
-    <Book 
-      img={firstBook.img} 
-      title={firstBook.title} 
-      author={firstBook.author} 
-    >
-      <p>
-        Show a child just how strong your love is every minute of the day! 
-        Features a "To" and "From" personalization page, making this sweet offering 
-        an ideal gift for baby showers, birthdays, and new parents. 
-      </p>
-    </Book>
-    <Book 
-      img={secondBook.img} 
-      title={secondBook.title} 
-      author={secondBook.author} 
-    />
+    {books.map((book) => {
+      return (<Book key={book.id} /* book={book} */ {...book} />)
+    })}
   </section>)  
 }
 
 const Book = (props) => {
-  const { img, title, author, children } =  props;
-  console.log(children);
+  const { img, title, author } =  props;
   return (
     <article className='book'>
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 }
